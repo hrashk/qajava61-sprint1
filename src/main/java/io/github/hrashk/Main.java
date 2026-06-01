8e9899ffcebc7ccea658d78653d67c5fb1c07c69
Jane Doe <jane.doe@example.com>
2026-06-01 17:32:55 +0000

    hello jane

--- src/main/java/io/github/hrashk/Main.java
+++ src/main/java/io/github/hrashk/Main.java
@@ -5,5 +5,6 @@ package io.github.hrashk;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome to Java!");
+	System.out.println("Hello Jane");
     }
 }
